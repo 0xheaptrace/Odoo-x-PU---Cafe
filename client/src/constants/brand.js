c41c0957409d63b36aad53bdfc3dd688de9348ea
Imsta1 <imstavan12@gmail.com>
2026-06-14 07:55:39 +0530

    Delete client directory

--- client/src/constants/brand.js
+++
@@ -1,3 +0,0 @@
-export const BRAND_NAME = 'Folk & Forks'
-export const BRAND_TAGLINE = 'Crafted for Every Craving'
-export const BRAND_POS_NAME = 'Folk & Forks'
--- client/src/services/paymentSettingsService.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-// Fetch current payment method settings (cash, card, UPI toggles)
-export const getPaymentSettings = () => api.get('/payment-settings')
-
-// Update payment method settings
-export const updatePaymentSettings = (data) =>
-  api.put('/payment-settings', data)
